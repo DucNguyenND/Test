66e8c7788a0541b0d5a00ec504788345b464b9f4
DucNguyenND <ducnguyenbvtvc@gmail.com>
2023-04-05 23:55:33 +0700

    up

---
+++ config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    introTitle: 'Em yêu à!',
+    // introDesc: `Anh yêu em Hoài`,
+    btnIntro: 'Dạ 🥰 ',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnNo: 'Không ạ :3',
+
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    btnAccept: 'Okiiiii  <3',
+    messLink: 'http://fb.com/duc85'
+}
